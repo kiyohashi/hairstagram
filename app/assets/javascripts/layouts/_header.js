6ebfb1e9e217e199017ba88eba4e32a47cb9532f
kiyohashi <iz.kk.r77@gmail.com>
2019-12-26 00:08:33 +0900

    user-menu create

---
+++ app/assets/javascripts/layouts/_header.js
@@ -0,0 +1,12 @@
+// $(function() {
+//   userMENU = document.getElementById("header-user-menu");
+//   $(".header__container__right").hover(
+//     function() {
+//       console.log(userMENU)
+//       userMENU.style.display="block";
+//     }
+//     ,function() {
+//       userMENU.style.display="none";
+//     }
+//   );
+// });
