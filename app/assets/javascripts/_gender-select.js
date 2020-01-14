4e8660b0f077b119dda6d4df25db3c1d773a1e30
kiyohashi <iz.kk.r77@gmail.com>
2020-01-15 02:11:03 +0900

    Update _gender-select.js

--- app/assets/javascripts/_gender-select.js
+++ app/assets/javascripts/_gender-select.js
@@ -3,7 +3,7 @@ $(function() {
     $(".gender-select__list li").removeClass('active');
 
     $(".ranking__contents > div").removeClass('active');
-    $(".timeline__posts > div").removeClass('active');
+    $(".timeline__main__posts > div").removeClass('active');
     var id_gender = $(this).attr('id')
     var posts = document.getElementsByClassName(id_gender)[0];
     $(posts).addClass('active')
