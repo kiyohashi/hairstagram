9c664e1d7c8369c8cf6a6fea2f86903613b9252e
kiyohashi <iz.kk.r77@gmail.com>
2020-01-15 17:55:22 +0900

    correction

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,16 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     favorite = Favorite.create(favorite_params)
     redirect_to request.referrer
-    # redirect_to :back
-    # redirect_to post_path(favorite.post.id)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
     favorite.destroy
-    # redirect_to post_path(@post)
     redirect_to request.referrer
-    # redirect_to :back
   end
 
   private
--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -373,6 +373,113 @@ class Scraping
   end
 
 
+  def self.centargaiEtcSalons_urls
+    agent = Mechanize.new
+    
+    links = [] 
+    next_url = ''
+    last_url = ''
+    
+    while true
+      current_page = agent.get('https://beauty.hotpepper.jp/svcSA/macAD/salon/sacX007/' + last_url)
+      
+      elements = current_page.search('.slcHeadContentsInner a')
+      elements.each do |ele|
+        links << ele.get_attribute(:href)
+      end
+      
+      next_link = current_page.at('.paging .afterPage a')
+      break unless next_link
+      next_url = next_link.get_attribute(:href)
+      next_urls = next_url.split("/")
+      last_url = next_urls[next_urls.length - 1]
+      sleep(1)
+    end
+    
+    links.each do |link|
+      get_salon(link)
+      sleep(1)
+    end
+  end
+
+  def self.get_salon(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    
+    name = page.at('.detailTitle a').inner_text if page.at('.detailTitle a')
+    nameKana = page.at('.sprtHeaderInner .fs10').inner_text if page.at('.sprtHeaderInner .fs10')
+    address =  page.at('.fs10 > li:first-child').inner_text if page.at('.fs10 > :first-child')
+    access =  page.at('.fs10 > li:last-child').inner_text if page.at('.fs10 > li:last-child')
+    image_url = page.at('.slnTopImgViewer img')[:src] if page.at('.slnTopImgViewer img')
+    introduction = page.at('.slnTopImgDescription p').inner_text if page.at('.slnTopImgDescription p')
+    
+    # first_or_initializeメソッドでbooksテーブルのタイトルカラムに特定のタイトルが存在しなければ新たにインスタンスを作成する
+    salon = Salon.where(name: name).first_or_initialize
+    salon.nameKana = nameKana
+    salon.address = address
+    salon.access = access 
+    salon.image_url = image_url
+    salon.introduction = introduction
+    salon.link = link
+    salon.area_id = 8
+    
+    salon.save
+  end
+
+  def self.miyamasuzakaEtcSalons_urls
+    agent = Mechanize.new
+    
+    links = [] 
+    next_url = ''
+    last_url = ''
+    
+    while true
+      current_page = agent.get('https://beauty.hotpepper.jp/svcSA/macAD/salon/sacX111/' + last_url)
+      
+      elements = current_page.search('.slcHeadContentsInner a')
+      elements.each do |ele|
+        links << ele.get_attribute(:href)
+      end
+      
+      next_link = current_page.at('.paging .afterPage a')
+      break unless next_link
+      next_url = next_link.get_attribute(:href)
+      next_urls = next_url.split("/")
+      last_url = next_urls[next_urls.length - 1]
+      sleep(1)
+    end
+    
+    links.each do |link|
+      get_salon(link)
+      sleep(1)
+    end
+  end
+
+  def self.get_salon(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    
+    name = page.at('.detailTitle a').inner_text if page.at('.detailTitle a')
+    nameKana = page.at('.sprtHeaderInner .fs10').inner_text if page.at('.sprtHeaderInner .fs10')
+    address =  page.at('.fs10 > li:first-child').inner_text if page.at('.fs10 > :first-child')
+    access =  page.at('.fs10 > li:last-child').inner_text if page.at('.fs10 > li:last-child')
+    image_url = page.at('.slnTopImgViewer img')[:src] if page.at('.slnTopImgViewer img')
+    introduction = page.at('.slnTopImgDescription p').inner_text if page.at('.slnTopImgDescription p')
+    
+    # first_or_initializeメソッドでbooksテーブルのタイトルカラムに特定のタイトルが存在しなければ新たにインスタンスを作成する
+    salon = Salon.where(name: name).first_or_initialize
+    salon.nameKana = nameKana
+    salon.address = address
+    salon.access = access 
+    salon.image_url = image_url
+    salon.introduction = introduction
+    salon.link = link
+    salon.area_id = 9
+    
+    salon.save
+  end
+
+
 
 
 
